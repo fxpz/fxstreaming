5fc4633c8a416bf01488d88e4f9d3107242abfb7
Fx <felice.pizzurro@gmail.com>
2015-12-08 00:01:37 +0100

    fixed regexes

--- settings.py
+++ settings.py
@@ -1,6 +1,31 @@
+import logging
+
+# LOGGING
+LOGDIR = '/tmp/'
+LOGFILE = 'fxlog'
+LOGLEVEL = logging.DEBUG
+
+# PSMANIACO
 PSM_PATH_PAGE_FILES = '/Users/baky/fxstreaming/files/psmaniaco'
 
-PSM_XPATH_HREF='//a[@href]'
-PSM_XPATH_CONTENT='//table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td'
-PSM_XPATH_TITLE='/html/head/title'
+PSM_XPATH_HREF = '//a[@href]'
+PSM_XPATH_CONTENT = '//table[@class="color"]'
+PSM_XPATH_TITLE = '/html/head/title'
 
+PSM_TITLE_FILTER_OUT = [
+    ('\[ita\]', ''),
+    ('serie\ tv', ''),
+    ('\ {2}', ' '),
+    ('-', '')
+]
+
+PSM_SEASON_NUMERIC_REGEX = [
+    (r'^(.*)(stagione|serie)\ (\d)+.*$', 1, 3),
+]
+
+PSM_SEASON_LITERAL_REGEX = [
+    (r'^(.*)(prima|seconda|terza|quarta|quinta|sesta|settima|'
+     'ottava|nona|decima)\ (stagione).*$', 1, 2),
+    (r'^(.*)(stagione)\ (uno|due|tre|quattro|cinque|sei|sette|'
+     'otto|nove|dieci).*$', 1, 3),
+]
