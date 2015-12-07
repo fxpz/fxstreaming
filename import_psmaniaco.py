ab0eb3dc9b8371112b1feba9235d27c0494ed0a8
Fx <felice.pizzurro@gmail.com>
2015-12-08 00:01:52 +0100

    .

--- import_psmaniaco.py
+++ import_psmaniaco.py
@@ -6,6 +6,8 @@ files = [x for x in os.listdir(settings.PSM_PATH_PAGE_FILES)
          if x.endswith('.html')]
 for f in files:
     p = PsManiaco(f)
-    print p.getRawTitle()
-    print p.getRawContent()
-    print "---------------------------\n"
+    print p.getTitle()
+    print p.getSeason()
+    p = p.getRawContent()
+    help(p)
+    print '-------------'
